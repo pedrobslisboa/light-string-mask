4cdd58ae55814b4a5b00b6734d0ee0ebde0ce3b6
Dmytro Chumak <hello@webman.pro>
2020-06-20 22:54:57 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
